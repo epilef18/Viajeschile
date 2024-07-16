89b8ef5fce598c34d5098d20fdd0ec12798022d2
epilef18 <f.villasanles@gmail.com>
2024-07-16 14:02:30 -0400

    commit_3_viajeschile

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#card1titulo").on("click", function () {
+        $("#card1text").toggle("fast", function () {});
+    });
+    $("#card2titulo").on("click", function () {
+     $("#card2text").toggle("fast", function () {});
+    });
+    $("#card3titulo").on("click", function () {
+    $("#card3text").toggle("fast", function () {});
+    });
+    $("#card4titulo").on("click", function () {
+    $("#card4text").toggle("fast", function () {});
+    });
+    //toggler de texto en cards, funcion activada con click en el título
+});
